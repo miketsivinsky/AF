ef467e6fcd4d5eebf42d42819828b87af97e0b6c
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2016-02-21 20:07:43 +0600

    move read_infile func from 'main' to alg_data_io module

--- alg/alg_preview.py
+++ alg/alg_preview.py
@@ -1,49 +1,18 @@
 import sys
 import os
-import numpy as np
-
-import time # for profiling only
+import importlib          # for debub only
+#import numpy as np
 
 #------------------------------------------------------------------------------
 sys.path.append(os.getcwd() + '/alglib')
+import alg_data_io as alg_io
+importlib.reload(alg_io)  # for debub only
 
 #------------------------------------------------------------------------------
 DATA_FILE    = 'frame[050][+0190].dat'
 AF_PATH      = os.environ['AF_PATH']
 infile_name  = os.path.join(AF_PATH,'data','in',DATA_FILE)
+out = alg_io.read_infile(infile_name)
 
-print(infile_name)
-
-#------------------------------------------------------------------------------
-header_t = np.dtype([
-    ("FileNum",          np.int32 ),
-    ("LensControlValue", np.int32 ),
-    ("ClassId",          np.uint32),
-    ("MsgClassId",       np.uint32),
-    ("NetSrc",           np.uint32),
-    ("NetDst",           np.uint32),
-    ("FrameNum",         np.uint32),
-    ("PixelSize",        np.uint32),
-    ("Height",           np.uint32),
-    ("Width",            np.uint32),
-])
-
-with open(infile_name, 'rb') as infile:
-    header = np.fromfile(infile, dtype = header_t, count = 1)
-    #-----------------
-    print(header)
-
-    start_time = time.time()                                        # for profiling only
-    frame = np.fromfile(infile, dtype = np.uint16)
-    frame = frame.reshape((header['Height'][0],header['Width'][0]))
-    d_time = time.time() - start_time                               # for profiling only
-    print("elapsed time {0:6.4f} ms".format(d_time*1000))           # for profiling only
-    print(frame.shape)
-
-
-    pass
-    #-----------------
-print(infile.closed)
-
-#------------------------------------------------------------------------------
+print(out.header)
 
--- alg/alglib/alg_data_io.py
+++ alg/alglib/alg_data_io.py
@@ -1,2 +1,39 @@
-def read_infile(file_name):
-    print(file_name)
+#------------------------------------------------------------------------------
+import numpy as np
+from collections import namedtuple
+import time            # for profiling only
+
+#------------------------------------------------------------------------------
+def read_infile(infile_name):
+    """ read input data flie (file: header,frame) """
+
+    # header format
+    header_t = np.dtype([
+        ("FileNum",          np.int32 ),
+        ("LensControlValue", np.int32 ),
+        ("ClassId",          np.uint32),
+        ("MsgClassId",       np.uint32),
+        ("NetSrc",           np.uint32),
+        ("NetDst",           np.uint32),
+        ("FrameNum",         np.uint32),
+        ("PixelSize",        np.uint32),
+        ("Height",           np.uint32),
+        ("Width",            np.uint32),
+    ])
+
+    #open file, read header and frame
+    with open(infile_name, 'rb') as infile:
+        header = np.fromfile(infile, dtype = header_t, count = 1)
+        #-----------------
+        start_time = time.time()                                        # for profiling only
+        frame = np.fromfile(infile, dtype = np.uint16)
+        frame = frame.reshape((header['Height'][0],header['Width'][0]))
+        d_time = time.time() - start_time                               # for profiling only
+        print("elapsed time {0:6.4f} ms".format(d_time*1000))           # for profiling only
+        # print(frame.shape)
+    #print(infile.closed) # debug
+
+    # return infile_data
+    infile_data = namedtuple("infile_data",["header","frame"])
+    return infile_data(header,frame)
+
