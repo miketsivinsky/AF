f2f54ee9d97b74285375530e12410477f3c4ad61
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2016-03-09 19:53:48 +0600

    show image test without pylab

--- alg/alg_preview.py
+++ alg/alg_preview.py
@@ -8,6 +8,10 @@ sys.path.append(os.getcwd() + '/alglib')
 import alg_data_io as alg_io
 importlib.reload(alg_io)  # for debub only
 
+# http://stackoverflow.com/questions/30809316/how-to-create-a-plot-in-matplotlib-without-using-pyplot
+import matplotlib as mpl 
+from matplotlib.backends import backend_tkagg as backend 
+
 #------------------------------------------------------------------------------
 FRAME_HEIGHT = 600
 FRAME_WIDTH  = 800
@@ -18,4 +22,11 @@ infile_name  = os.path.join(AF_PATH,'data','in',DATA_FILE)
 out = alg_io.read_infile(infile_name, Width = FRAME_WIDTH, Height = FRAME_HEIGHT)
 
 print(out.header, out.frame_size_valid)
+
+#------------------------------------------------------------------------------
+fig = mpl.figure.Figure()
+backend.new_figure_manager_given_figure(1,fig)
+ax  = fig.add_axes([0, 0, 1, 1])
+ax.imshow(out.frame, cmap = plt.cm.gray)
+fig.show()
 
