e96624d218f6199d67fd12727e5a1f91928e58fa
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2016-03-17 14:36:22 +0600

    test screen metrics and windows control

--- alg/alg_preview.py
+++ alg/alg_preview.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+import screeninfo
+
 #------------------------------------------------------------------------------
 sys.path.append(os.getcwd() + '/alglib')
 import alg_data_io as alg_io
@@ -24,12 +26,31 @@ out = alg_io.read_infile(infile_name, Width = FRAME_WIDTH, Height = FRAME_HEIGHT
 print(out.header, out.frame_size_valid)
 
 #------------------------------------------------------------------------------
-fig = plt.figure()
-ax  = fig.add_axes([0, 0, 1, 1])
-ax.imshow(out.frame, cmap = plt.cm.gray)
-#ax.imshow(out.frame, cmap = plt.cm.gray, extent = [0, 800, 600, 0])
-ax.plot([10,100], [20,200])
-fig.show()
+# see Python 3.5\Lib\site-packages\matplotlib\backend_bases.py - func mouse_move(self, event) for x,y [pixel value] ena/dis control
 
+fig = plt.figure(frameon = True)
+fig.canvas.set_window_title('slon')
+ax  = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+ax.autoscale(tight = True)
+ax.set_axis_off()                         # or use #ax.xaxis.set_visible(False) + #ax.yaxis.set_visible(False)
+ax.format_coord = lambda x, y: ''
+#ax.set_navigate(False)                   # disable navigate toolbar functions and disable coord and image pixel value dispaying
+img = ax.imshow(out.frame, cmap = plt.cm.gray, origin = 'upper')
+#img.format_cursor_data = lambda data: '' # not need
+img.get_cursor_data = lambda event: None
+ax.plot([10,100], [20,200])
+
+
+#--------------------------
+mng = fig.canvas.manager
+mon = screeninfo.get_monitors()
+print(mon)
+mng.window.wm_geometry("+"+str(mon[0].x)+"+0")
+#mng.full_screen_toggle()
+mng.window.state('zoomed')
+#mng.set_window_title('slonick')
+
+#--------------------------
+#ax.figure.show()
 
 
